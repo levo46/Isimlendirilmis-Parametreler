f53467f8371fab9cdef2e1354590899064983ba2
levent581 <levent581@BPC03>
2023-12-05 09:21:26 +0300

    Proje dosyası ekle.

---
+++ Isimlendirilmis Parametreler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isimlendirilmis_Parametreler
+{
+    class SayiIslemler
+    {
+        public int Topla(int Sayi1, int Sayi2,int Sayi3)
+        {
+            return (Sayi1 + Sayi2 + Sayi3);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayiIslemler sayiIslemler = new SayiIslemler();
+            int Topla=sayiIslemler.Topla(Sayi1:10,Sayi2:5,Sayi3:15);
+            Console.WriteLine("Toplam:{0}",Topla);
+        }
+    }
+}
